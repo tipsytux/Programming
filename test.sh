982c1438f0b20af8dea46be7f1bf7a0a7771d0db
cse001 <aadityapcm@gmail.com>
2021-09-29 13:28:08 +0530

    Modified :M README.md Added :  tempFile Modified :  test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,8 @@
-for LINE in $(cat README.md)
+tree -a -I '.git|.idea|out|Programming.iml' -t -f | sed '$ d' | sed 's/`/|/g' |  sed '$ d' > tempReadMe
+awk 'NF>1{print $NF}' README.md > tempFile
+sed -i 's/\(.*\)/\(\1\)/' tempFile
+sed -i 's/.\///g' tempFile
+for LINE in $(cat tempFile)
 do
     echo "$LINE"
 done
