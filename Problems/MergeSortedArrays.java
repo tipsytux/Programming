27d84248125d0e982f38d694a49ca5f1e9f4c518
cse001 <aadityapcm@gmail.com>
2021-09-29 17:38:09 +0530

    Modified :  Problems/MergeSortedArrays.java

--- Problems/MergeSortedArrays.java
+++ Problems/MergeSortedArrays.java
@@ -40,7 +40,31 @@ import java.util.Scanner;
 public class MergeSortedArrays {
     public static int[] mergeBothInDescendingOrder(int[] array1, int[] array2, int size1, int size2){
         int[] sortedArray = new int[size1+size2];
-        int left = 
+        int left = size1-1;
+        int right = size2-1;
+        int i = 0;
+        while (left >= 0 && right >=0){
+            if (array1[left] > array2[right]){
+                sortedArray[i] = array1[left];
+                left--;
+                i++;
+            }
+            else{
+                sortedArray[i] = array2[right];
+                right--;
+                i++;
+            }
+        }
+        while(left >=0){
+            sortedArray[i] = array1[left];
+            left--;
+            i++;
+        }
+        while(right>=0){
+            sortedArray[i] = array2[right];
+            right--;
+            i++;
+        }
         return sortedArray;
     }
     public static void main(String[] args) {
@@ -54,6 +78,10 @@ public class MergeSortedArrays {
         int[] array2 = new int[size2];
         for(int i=0;i<size2;i++) {
             array2[i] = scanner.nextInt();
+        }
+        int[] sortedArray = mergeBothInDescendingOrder(array1,array2,size1,size2);
+        for (int number : sortedArray){
+            System.out.println(number);
         }
     }
 }
