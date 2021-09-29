c6ae24cdaf7cae030f42ee6c51d93d65cb3d9f07
cse001 <aadityapcm@gmail.com>
2021-09-29 16:58:09 +0530

    Modified :  Sorting/Alg3_InsertionSort.java Modified :  Sorting/Alg4_MergeSort.java

--- Sorting/Alg4_MergeSort.java
+++ Sorting/Alg4_MergeSort.java
@@ -18,18 +18,55 @@ public class Alg4_MergeSort {
             mergeSort(numbers, first, mid);
             mergeSort(numbers, mid + 1, last);
 
-            merge(numbers, first, mid, last);
+            mergeAscending(numbers, first, mid, last);
         }
 
         return numbers;
     }
+    private static int[] mergeDescending(int[] numb, int start, int mid, int end) {
+        int left = start; //inital index of first subarray
+        int right = mid + 1; // initial index of second subarray
+        int k = 0; // initial index of merged array
+        int[] temp = new int[numb.length];
+        while (left <= mid && right <= end) {
+            if (numb[left] >= numb[right]) {
+                temp[k] = numb[left];
+                k++;
+                left++;
+            } else {
+                temp[k] = numb[right];
+                k++;
+                right++;
+            }
+        }
+        // Copy the remaining elements on left half , if there are any
+        while (left <= mid) {
+            temp[k] = numb[left];
+            k++;
+            left++;
+        }
+        // Copy the remaining elements on right half , if there are any
+        while (right <= end) {
+            temp[k] = numb[right];
+            k++;
+            right++;
+        }
+        // Copy the remaining elements from array t back the numbers array
+        left = start;
+        k = 0;
+        while (left <= end) {
+            numb[left] = temp[k];
+            left++;
+            k++;
+        }
+        return numb;
+    }
 
-    private static int[] merge(int[] numbers, int i, int m, int j) {
+    private static int[] mergeAscending(int[] numbers, int i, int m, int j) {
         int l = i; //inital index of first subarray
         int r = m + 1; // initial index of second subarray
         int k = 0; // initial index of merged array
         int[] t = new int[numbers.length];
-
         while (l <= m && r <= j) {
             if (numbers[l] <= numbers[r]) {
                 t[k] = numbers[l];
@@ -41,21 +78,18 @@ public class Alg4_MergeSort {
                 r++;
             }
         }
-
         // Copy the remaining elements on left half , if there are any
         while (l <= m) {
             t[k] = numbers[l];
             k++;
             l++;
         }
-
         // Copy the remaining elements on right half , if there are any
         while (r <= j) {
             t[k] = numbers[r];
             k++;
             r++;
         }
-
         // Copy the remaining elements from array t back the numbers array
         l = i;
         k = 0;
@@ -64,7 +98,6 @@ public class Alg4_MergeSort {
             l++;
             k++;
         }
-
         return numbers;
     }
 
@@ -75,6 +108,3 @@ public class Alg4_MergeSort {
         System.out.println(Arrays.toString(sortedNumbers));
     }
 }
-/*
-We are good to go...
- */
