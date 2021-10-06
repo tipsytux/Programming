4cde90564131ede0be1147d1fc0528ce8c2a51fa
cse001 <aadityapcm@gmail.com>
2021-10-06 22:18:37 +0530

    Modified :  Test.java

--- Test.java
+++ Test.java
@@ -2,18 +2,31 @@ import java.util.*;
 
 public class Test {
 
-    public static void main(String arg[]) {
-        Queue<Character> q=new LinkedList<>();
-        System.out.println(q.isEmpty());
-        q.add('a');
-        q.add('b');
-        q.add('c');
-        q.add('d');
-        System.out.println(q);
-        System.out.println(q.size());
-        System.out.println(q.peek());
-        System.out.println(q.remove());
-        System.out.println(q);
-        System.out.println(q.isEmpty());
+    public static void main(String[] args) {
+        Queue numbers = new LinkedList();
+        List(numbers);
+
+        // Write your code here
+        // You should only add some numbers, dequeue, or peek to change the elements in the queue
+        // so that the queue becomes [1,4,9,14,15]
+        while(!numbers.isEmpty()){
+            numbers.remove();
+        }
+        numbers.add(1);
+        numbers.add(4);
+        numbers.add(9);
+        numbers.add(14);
+        numbers.add(15);
+        System.out.println(numbers);
+    }
+
+    public static void List(Queue numbers) {
+        int i = 0;
+        while(i <= 12) {
+            if(i%2==0) {
+                numbers.add(i);
+            }
+            i++;
+        }
     }
 }
