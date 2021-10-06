495cf3b8ee44bf74d3f0b01b7f7fb7c13467aa36
cse001 <aadityapcm@gmail.com>
2021-10-06 21:08:37 +0530

    Added :  DataStructures/Stack/ReverseElements.java Modified :  README.md

---
+++ DataStructures/Stack/ReverseElements.java
@@ -0,0 +1,26 @@
+package DataStructures.Stack;
+
+/**
+ * Description
+ * You are given a stack with n integers.
+ * You need to reverse the last half of the elements i.e. floor(n/2) elements
+ * from the bottom of the stack using recursion and print the elements of the stack.
+ * Note : Floor function takes input as a real number and it returns the greatest integer less than or equal to the input.
+ * Ex. floor(5.4) will give output as 5 and floor(4) will give 4.
+ *
+ * Example:
+ * Stack(bottom -> top) = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+ * Output: [1, 2, 3, 4, 5, 6, 12, 11, 10, 9, 8, 7]
+ *
+ * Stack(bottom -> top) = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+ * Output: [1, 2, 3, 4, 5, 6, 11, 10, 9, 8, 7]
+ *
+ * Input Format:
+ * 12
+ * 1 2 3 4 5 6 7 8 9 10 11 12
+ *
+ * Output format:
+ * [1, 2, 3, 4, 5, 6, 12, 11, 10, 9, 8, 7]
+ */
+public class ReverseElements {
+}
