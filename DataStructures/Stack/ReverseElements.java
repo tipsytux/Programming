968000ebde91401f5e8e1baf74b5fb43e3518851
cse001 <aadityapcm@gmail.com>
2021-10-06 21:18:37 +0530

    Modified :  DataStructures/Stack/ReverseElements.java

--- DataStructures/Stack/ReverseElements.java
+++ DataStructures/Stack/ReverseElements.java
@@ -1,5 +1,5 @@
 package DataStructures.Stack;
-
+import java.util.*;
 /**
  * Description
  * You are given a stack with n integers.
@@ -23,4 +23,18 @@ package DataStructures.Stack;
  * [1, 2, 3, 4, 5, 6, 12, 11, 10, 9, 8, 7]
  */
 public class ReverseElements {
+    public static void main(String args[]) {
+        Stack<Integer> stack = new Stack<>();
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        while (n-- > 0)
+            stack.push(s.nextInt());
+        reverseSecondHalf(stack);
+    }
+
+    // Method to reverse the last half of the elements from the bottom of the stack
+    static void reverseSecondHalf(Stack<Integer> stack) {
+        // Write your code here
+        Stack<Integer> newStack = stack.clone();
+    }
 }
