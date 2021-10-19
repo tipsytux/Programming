521e5ebbd3716f6191f961e5bc3d3836721890fe
cse001 <aadityapcm@gmail.com>
2021-10-19 18:58:32 +0530

    Modified :  Source.java

--- Source.java
+++ Source.java
@@ -1,37 +1,19 @@
 import java.util.*;
 
 class Source {
+
     public static void main(String[] args) {
-        int[] array = new int[10];
-        array[0]=5;
-        array[1]= 8;
-        array[2]=4;
-        array[3]=4;
-        array[4]=7;
-        array[5]=6;
-        array[6]=2;
-        array[7]=6;
-        array[8]= 7;
-        array[9]=3;
-        method(array);
-    }
-    public static void method(int[]  array ){
-        int count = 0,n = array.length;
-
-        HashSet<Integer> hashSet = new HashSet<Integer>();
-
-        for (int i = 0; i < n; i++)
-            hashSet.add(array[i]);
-
-        int currentElement = array[0];
-
-        while (hashSet.contains(currentElement) == true) {
-
-            count++;
-
-            currentElement--;
+        Scanner in = new Scanner(System.in);
+        int n;
+        n = in.nextInt();
+        //array to store the input elements
+        int[] array = new int[n];
+        //storing the elements to the array
+        for (int i = 0; i < n; i++) {
+            array[i] = in.nextInt();
         }
+        // Write your code here
+        HashSet
 
-        System.out.println(count);
     }
 }
