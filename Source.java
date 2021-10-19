370dc81f4a21ecdd8b8420efc5468c8708ea1d4c
cse001 <aadityapcm@gmail.com>
2021-10-20 01:58:31 +0530

    Modified :  Source.java

--- Source.java
+++ Source.java
@@ -1,42 +1,31 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
-class Source {
-
-    public static void main(String[] args) {
+class Source
+{
+    public static void main (String[] args)
+    {
         Scanner in = new Scanner(System.in);
-        int n;
-        n = in.nextInt();
-        //array to store the input elements
-        int[] array = new int[n];
-        //storing the elements to the array
-        for (int i = 0; i < n; i++) {
+        // number of the elements
+        int n = in.nextInt();
+        int array[] = new int[n];
+        //storing the input integers to an array
+        for(int i =0;i<n;i++){
             array[i] = in.nextInt();
         }
-        // Write your code here
-        TreeSet hashSet = new TreeSet();
-        for (int a : array){
-            hashSet.add(a);
-        }
-
-        int size = 0;
-        int temp=0;
-        int current=0;
-        for (Object a : hashSet){
-            current = Integer.parseInt(a.toString());
-            if(size==0){
-                size = 1;
+        // getting the target sum from input
+        int targetSum = in.nextInt();
+        //write your code here
+        Arrays.sort(array);
+        boolean found = false;
+        for(int i=0;i<array.length;i++){
+            for(int j=i+1;j<array.length;j++){
+                if(array[i]+array[j]==targetSum){
+                    found = true;
+                }
             }
-            else{
-                size = size + current - temp;
-            }
-            temp = current;
         }
-        if(size == hashSet.size() && n!=0){
-            System.out.println(true);
-        }
-        else{
-            System.out.println(false);
-        }
+        System.out.println(found);
 
     }
 }
