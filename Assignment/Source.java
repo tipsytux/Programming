0e08f5df74cbbef9f942b58ab12ee669153b207a
cse001 <aadityapcm@gmail.com>
2021-10-27 06:34:10 +0530

    Modified :  Assignment/Source.java

--- Assignment/Source.java
+++ Assignment/Source.java
@@ -9,46 +9,30 @@ public class Source {
         int j = 0;
         int count;
         int m1 = -1, m2 = -1;
-
-        /* Since there are 2n elements, median will
-           be average of elements at index n-1 and
-           n in the array obtained after merging ar1
-           and ar2 */
         for (count = 0; count <= n; count++)
         {
-            /* Below is to handle case where all
-              elements of ar1[] are smaller than
-              smallest(or first) element of ar2[] */
             if (i == n)
             {
                 m1 = m2;
-                m2 = ar2[0];
+                m2 = arr2[0];
                 break;
             }
-
-            /* Below is to handle case where all
-               elements of ar2[] are smaller than
-               smallest(or first) element of ar1[] */
             else if (j == n)
             {
                 m1 = m2;
-                m2 = ar1[0];
+                m2 = arr1[0];
                 break;
             }
-            /* equals sign because if two
-               arrays have some common elements */
-            if (ar1[i] <= ar2[j])
+            if (arr1[i] <= arr2[j])
             {
-                /* Store the prev median */
                 m1 = m2;
-                m2 = ar1[i];
+                m2 = arr1[i];
                 i++;
             }
             else
             {
-                /* Store the prev median */
                 m1 = m2;
-                m2 = ar2[j];
+                m2 = arr2[j];
                 j++;
             }
         }
