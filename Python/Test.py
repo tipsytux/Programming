4450e7932e037d3c8661a13175f4e4e8ffd89ee7
cse001 <aadityapcm@gmail.com>
2021-11-21 04:16:41 +0530

    Modified :  Python/Test.py

--- Python/Test.py
+++ Python/Test.py
@@ -1,2 +1,20 @@
-string = "OQYWFClFhFGAvIWYwGKpmZhnJiyzTslSIhSwvOsqJMEphzmifTkyqOMNpnOtXZxmCfgDYqbaBHAUvIWhMnvwZnEMVDvmEfLrDoQnAZgQEgXQVnmSYkfedpAdhrtpOgORpYLRZYGWdhWYuqQssCUXtTzKRDAhpjUheOzUroZNzWFtZOVwIapzUYtbSbjYNErzQ"
-print(len(string))
+class A:
+    x = 10
+
+    def __init__(self, y, z):
+        self.y = y
+        self.z = z
+
+    def update_y(self):
+        self.y = self.y * self.x
+
+    @classmethod
+    def update_z(cls):
+        self.z = self.z + 20
+
+
+A1 = A(3, 4)
+A2 = A(5, 6)
+
+A2.update_z()
+print(A1.y + A2.z)
